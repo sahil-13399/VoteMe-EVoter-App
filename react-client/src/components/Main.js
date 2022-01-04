5b83f2f062e7fcb1afdc1e0a1281362274f706ad
Sahil Samantaray <sahil13399@gmail.com>
2022-01-04 12:38:13 +0530

    Main page added

---
+++ react-client/src/components/Main.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {useState} from 'react'
+import './Main.css'
+
+
+function Main() {
+
+    const [id, setId] = useState("");
+
+    return (
+        <div className='main_page'>
+            Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+        </div>
+    )
+}
+
+export default Main
