7fcea525f9fd6e4170f2ff541ddff4d7f088e7cb
Sahil Samantaray <sahil13399@gmail.com>
2022-01-04 12:50:09 +0530

    Added bootstrap text box

--- react-client/src/components/Main.js
+++ react-client/src/components/Main.js
@@ -1,34 +1,31 @@
 import React from 'react'
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 import './Main.css'
-
+import Form from 'react-bootstrap/Form'
+import {Button} from 'react-bootstrap'
 
 function Main() {
 
     const [id, setId] = useState("");
+    const submitCallback = (e) => {
+        e.preventDefault();
+        //Call backaend with Event ID
+    }
 
     return (
         <div className='main_page'>
-            Form>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
-
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
+            <Form onSubmit={submitCallback}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>To vote, please type in the Event ID</Form.Label>
+                    <Form.Control type="text" placeholder="Enter Event ID" onChange={(e) => setId(e.target.value)}/>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+            <div className='new_vote'>
+                <h3>To create a new Voting event please Click <a href="" >here</a></h3>
+            </div>
         </div>
     )
 }
